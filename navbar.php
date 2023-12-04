f6e1fa70d2c47bb4190a4e817795c4aecb6c0266
Athar Rayyan Muhammad <132254139+Athraynmhd@users.noreply.github.com>
2023-12-04 15:50:25 +0700

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
   <title>Kos Finder</title>
+  <link rel="icon" href="../images/logo.png" type="image/png">
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
